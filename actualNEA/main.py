d65813d5aacc77c4da0939d61a8b943a1e2d2528
Ghufran <ghugluq2t@gmail.com>
2024-11-24 15:34:38 +0000

    added a /home just for ssing

--- actualNEA/main.py
+++ actualNEA/main.py
@@ -5,54 +5,41 @@ app = Flask(__name__)
 
 
 def loadnames():
-    conn = sqlite3.connect("recipes.db")
+    conn = sqlite3.connect("recipes.db")#connect to database
     c = conn.cursor()
-    c.execute("SELECT recipe_name FROM tableofrecipes2")
-    list = c.fetchall()
-    conn.close()
-    recipenames = []
-    count = 0
-    for item in list:
-        item = list[count]
-        item = item[0]
-        recipenames.append(item)
-        count += 1
-    return recipenames
+    c.execute("SELECT recipe_name FROM tableofrecipes2")#get all recipes from the table
+    allrecipes = c.fetchall()#this is a 2D list containing tuples, e.g. [(recipe1,), (recipe2,)]
+    conn.close()#close the connection to avoid errors
+    #re-format the list of recipes
+    formattedrecipes = []#prepare another list
+    count = 0#prepare a count
+    for item in allrecipes:#cycle through each tuple
+        item = allrecipes[count]
+        item = item[0]#get first item in list
+        formattedrecipes.append(item)#put into another list
+        count += 1#go to next tuple
+    return formattedrecipes
 
-
+@app.route('/home')
+def home():
+    recipenames = loadnames()
+    return render_template("index.html", recipenames=recipenames)#return all the recipes (passing the list into the frontend)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    conn = sqlite3.connect("recipes.db")
-    c = conn.cursor()
-
-    c.execute("SELECT recipe_name FROM tableofrecipes2")
-    row = c.fetchall()
-    conn.close()
-    row = list(row)
-    count = 0
-    newlist = []
-    for item in row:
-        item = row[count]
-        item = item[0]
-        newlist.append(item)
-        count += 1
-    #print(f"finished list: {newlist}")#i have removed the tuples
-
-    query = request.args.get("Query", '')#the '' is a default value.
-    recipenames = loadnames()
-    newrecipelist = []
-    for item in recipenames:
+    query = request.args.get("Query", '')#get the user input for the search box
+    recipenames = loadnames()#get every recipe from database
+    newrecipelist = []#prepare a new list
+    for item in recipenames:#cycle through every recipe
         if item:#checks if its empty
-            if query.upper() in item.upper():#converts the query to uppercase and each name in the list to uppercase and sees if the name contains the query
+            if query.upper() in item.upper():#converts the query to uppercase and each name in the list to uppercase and checks if the name contains the query
                 newrecipelist.append(item)#if it does, it adds it to the list
 
     if request.method == "POST":
         recipename = request.form.get('recipebutton')
         return render_template("recipe.html", recipename=recipename)
 
-    return render_template("index.html", row=row, newlist=newlist, newrecipelist=newrecipelist)
-
+    return render_template("index.html", newrecipelist=newrecipelist)#returns list containing the query
 
 @app.route('/<recipename>')
 def item(recipename):
