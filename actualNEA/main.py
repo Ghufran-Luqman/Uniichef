c029c5522ba8f4a943803fd319353c5d1306711d
N01meow <88330113+N01meow@users.noreply.github.com>
2024-11-26 10:01:17 +0000

    just saving idk if i did anything lol

--- actualNEA/main.py
+++ actualNEA/main.py
@@ -52,16 +52,16 @@ def home():
     return render_template("home.html", recipenames=recipenames, alert=alert)#return all the recipes if no query or if there are no recipes in the newrecipelist
 
 @app.route('/<recipename>')
-def recipe(recipename):
-    item=""
+def recipe(recipename):#gets recipe name
+    item=""#avoids an error
     conn = sqlite3.connect('recipes.db')
     c = conn.cursor()
-    c.execute("SELECT ingredients FROM tableofrecipes2 WHERE recipe_name = ?", (recipename,))
+    c.execute("SELECT ingredients FROM tableofrecipes2 WHERE recipe_name = ?", (recipename,))#gets ingredients
     ingredients = c.fetchall()
     ingredientlist = []
     for item in ingredients:
         item = item[0]
-        ingredientlist = [ingredient.strip() for ingredient in item.split(',')]#turns it into a list
+        ingredientlist = [ingredient.strip() for ingredient in item.split(',')]#turns it into a list for formatting
     return render_template('recipe.html', recipename=recipename, ingredientlist=ingredientlist)
 
 
